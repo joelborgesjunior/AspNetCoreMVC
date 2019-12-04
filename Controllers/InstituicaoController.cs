85afc75090af1b6f91c662a3f2f5b1dd4ed291fa
Joel Borges Junior <joelborgesjunior@hotmail.com>
2019-12-04 20:46:00 -0300

    Adicionando primeiros capitulos

---
+++ Controllers/InstituicaoController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreMVC.Models;
+
+
+namespace AspNetCoreMVC.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static List<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                IdInstituicao = 1,
+                NomeInstituicao = "USP",
+                EndereçoInstituicao = "São Paulo"
+            },
+            new Instituicao()
+            {
+                IdInstituicao = 2,
+                NomeInstituicao = "UFRJ",
+                EndereçoInstituicao = "Rio de Janeiro"
+            },
+            new Instituicao()
+            {
+                IdInstituicao = 3,
+                NomeInstituicao = "UFMG",
+                EndereçoInstituicao = "Minas Gerais"
+            },
+            new Instituicao()
+            {
+                IdInstituicao = 4,
+                NomeInstituicao = "UFSC",
+                EndereçoInstituicao = "Santa Catarina"
+            },
+            new Instituicao()
+            {
+                IdInstituicao = 5,
+                NomeInstituicao = "Mackenzie",
+                EndereçoInstituicao = "São Paulo"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Instituicao instituicao) 
+        {
+            instituicoes.Add(instituicao);
+            instituicao.IdInstituicao = instituicoes.Select(i => i.IdInstituicao).Max() + 1;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Instituicao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC.Models
+{
+    public class Instituicao
+    {
+        public long? IdInstituicao { get; set; }
+        public string NomeInstituicao { get; set; }
+        public string EndereçoInstituicao { get; set; }
+    }
+}
